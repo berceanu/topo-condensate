3f87f61c3427c90fd3bdd610c2116e7bd9a81d45
Andrei Berceanu <berceanu@runbox.com>
2015-05-25 21:23:43 +0200

    optimized equilibrium rhs function

--- runge-kutta.jl
+++ runge-kutta.jl
@@ -1,42 +1,37 @@
 using ODE
 
 
-m(l::Int,N::Int) = div(l-1,N) - div(N-1,2)
-n(l::Int,N::Int) = div(N-1,2) - rem(l-1,N)
+μ(l::Int,N::Int) = div(l-1,N) - div(N-1,2)
+ν(l::Int,N::Int) = div(N-1,2) - rem(l-1,N)
 
 
-cl(N::Int,l::Int,m₀::Int,n₀::Int) = (m(l,N) - m₀)^2 + (n(l,N) - n₀)^2
-
+cl(m::Int,n::Int,m₀::Int,n₀::Int) = (m - m₀)^2 + (n - n₀)^2
 
 
 # TODO: write new tests for equilibrium case
-# TODO: calculate m and n corresponding to given l only once
+
 
 function F(t::Float64,a::Array{Complex{Float64}, 1}, N::Int,p::Int,q::Int,m₀::Int,n₀::Int,J::Float64,σ::Float64,Ω::Float64,κ::Float64,f::Array{Float64,1})
     d = N^2
     adot = Array(Complex{Float64}, d) 
 
+    ϖ = im*J 
     for l = 1:d
-        #p3 and p4 are always present
-        denominator = im
-        p₃ = κ*cl(N,l,m₀,n₀) / denominator
-        p₄ = 1 / denominator
-        adot[l] = p₃*a[l] + p₄*abs2(a[l])*a[l]
+        m = μ(l::Int,N::Int)
+        n = ν(l::Int,N::Int)
+
+        p₃ = -im*κ*cl(m,n,m₀,n₀)
+
+        adot[l] = p₃*a[l] - im*abs2(a[l])*a[l]
         if l > N
-            p₁ = -J / denominator
-            p₂ = -J / denominator
-            adot[l] += p₁*a[l-N] + p₂*a[l-1]
+            adot[l] += ϖ*a[l-N] + ϖ*a[l-1]
         elseif l > 1
-            p₂ = conj(-J) / denominator
-            adot[l] += p₂*a[l-1]
+            adot[l] += ϖ*a[l-1]
         end 
         if l < d+1-N
-            p₅ = -J / denominator
-            p₆ = -J / denominator
-            adot[l] += p₅*a[l+1] + p₆*a[l+N]
+            adot[l] += ϖ*a[l+1] + ϖ*a[l+N]
         elseif l < d
-            p₅ = -J / denominator
-            adot[l] += p₅*a[l+1]
+            adot[l] += ϖ*a[l+1]
         end
     end
     adot
@@ -71,70 +66,76 @@ end
 ## p = 1
 ## q = 11
 
+
+
+### --->
+## N = 3
 # N = 65
-N = 3
 
-f = ones(N^2)
-Ω = 1.
+## f = ones(N^2)
+## Ω = 1.
 
 
-a₀ = ones(Complex{Float64}, N^2)
+## a₀ = ones(Complex{Float64}, N^2)
 
-σ = 0.
-J = 1.
-κ = 10.
+## σ = 0.
+## J = 1.
+## κ = 10.
 
-m₀ = 0
-n₀ = 0
+## m₀ = 0
+## n₀ = 0
 
-p = 0
-q = 1
-
+## p = 0
+## q = 1
+### <---
 
 ## tout, aout = odeXX((t,a)->F(t,a), a0, tspan; keywords...)
 ## F(t,a, N,p,q,m₀,n₀,J,σ,Ω,κ,f)
 
 
 
-tout, aout = ode45((t,a)->F(t,a, N,p,q,m₀,n₀,J,σ,Ω,κ,f), a₀, [0., 10], points=:specified)
+### tout, aout = ode45((t,a)->F(t,a, N,p,q,m₀,n₀,J,σ,Ω,κ,f), a₀, [0., 10], points=:specified)
 
 
 #a = sqrt(γ0/Γ0)*aout
 #t = 2ℏ*Γ0*tout/(U0*γ0)
 
+### --->
 #plotting
-using PyPlot
+## using PyPlot
 
-# matplotlib parameters
-matplotlib["rcParams"][:update](["axes.labelsize" => 22,
-                                 "axes.titlesize" => 20,
-                                 "font.size" => 18,
-                                 "legend.fontsize" => 14,
-                                 "axes.linewidth" => 1.5,
-                                 "font.family" => "serif",
-                                 "font.serif" => "Computer Modern Roman",
-                                 "xtick.labelsize" => 20,
-                                 "xtick.major.size" => 5.5,
-                                 "xtick.major.width" => 1.5,
-                                 "ytick.labelsize" => 20,
-                                 "ytick.major.size" => 5.5,
-                                 "ytick.major.width" => 1.5,
-                                 "text.usetex" => true,
-                                 "figure.autolayout" => true])
+## # matplotlib parameters
+## matplotlib["rcParams"][:update](["axes.labelsize" => 22,
+##                                  "axes.titlesize" => 20,
+##                                  "font.size" => 18,
+##                                  "legend.fontsize" => 14,
+##                                  "axes.linewidth" => 1.5,
+##                                  "font.family" => "serif",
+##                                  "font.serif" => "Computer Modern Roman",
+##                                  "xtick.labelsize" => 20,
+##                                  "xtick.major.size" => 5.5,
+##                                  "xtick.major.width" => 1.5,
+##                                  "ytick.labelsize" => 20,
+##                                  "ytick.major.size" => 5.5,
+##                                  "ytick.major.width" => 1.5,
+##                                  "text.usetex" => true,
+##                                  "figure.autolayout" => true])
 
-edge = div(N-1,2)
+## edge = div(N-1,2)
 
 
+## f, ax = plt.subplots(figsize=(5, 5))
+## ax[:imshow](abs2(reshape(aout[end], N,N)),
+##             origin="upper", ColorMap("gist_heat_r"), interpolation="none",
+##             extent=[-edge, edge, -edge, edge])
 
-f, ax = plt.subplots(figsize=(5, 5))
-ax[:imshow](abs2(reshape(aout[end], N,N)),
-            origin="upper", ColorMap("gist_heat_r"), interpolation="none",
-            extent=[-edge, edge, -edge, edge])
+## ax[:set_xlabel](L"$m$")
+## ax[:set_ylabel](L"$n$")
+## ax[:set_xticks]([-edge,0, edge])
+## ax[:set_yticks]([-edge,0, edge])
+### <---
 
-ax[:set_xlabel](L"$m$")
-ax[:set_ylabel](L"$n$")
-ax[:set_xticks]([-edge,0, edge])
-ax[:set_yticks]([-edge,0, edge])
+
 
 
 #testing
@@ -182,10 +183,10 @@ const C = [0.0,
 # @time for i=1:10^6; F(1.,ones(Complex{Float64}, 3^2), 3,1,11,0,0,1.,1.,1.,1.,ones(3^2)) ; end
 # 2.6s
 
-Profile.clear()
-@profile (for i = 1:10^6; F(1.,ones(Complex{Float64}, 3^2), 3,1,11,0,0,1.,1.,1.,1.,ones(3^2)); end)
+## Profile.clear()
+## @profile (for i = 1:10^6; F(1.,ones(Complex{Float64}, 3^2), 3,1,11,0,0,1.,1.,1.,1.,ones(3^2)); end)
 
-@time for i=1:100; F(1.,ones(Complex{Float64}, 512^2), 512,1,11,0,0,1.,1.,1.,1.,ones(512^2)) ; end
+#@time for i=1:100; F(1.,ones(Complex{Float64}, 512^2), 512,1,11,0,0,1.,1.,1.,1.,ones(512^2)) ; end
 # 10.9s
 
 ## Profile.clear()
