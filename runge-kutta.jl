7718471f16caae60c9e078db62689677244c15be
Andrei Berceanu <berceanu@runbox.com>
2015-05-26 21:31:26 +0200

    something coming out

--- runge-kutta.jl
+++ runge-kutta.jl
@@ -7,9 +7,9 @@ using ODE
 
 cl(m::Int,n::Int,m₀::Int,n₀::Int) = (m - m₀)^2 + (n - n₀)^2
 
-function F(t::Float64,a::Array{Complex{Float64}, 1}, N::Int,m₀::Int,n₀::Int,σ::Float64)
+function F(t::Float64,a::Array{Float64, 1}, N::Int,m₀::Int,n₀::Int,σ::Float64)
     d = N^2
-    adot = Array(Complex{Float64}, d) 
+    adot = Array(Float64, d) 
 
     for l = 1:d
         m = μ(l::Int,N::Int)
@@ -34,8 +34,51 @@ end
 
 const N = 20
 
+
+
 @time tout, aout = ode45((t,a)->F(t,a, N, 0,0, 25.),
-ones(Complex{Float64}, N^2), [0., 1.], points=:specified)
+ones(Float64, N^2)/N^2, linspace(0,15,16), points=:specified)
+
+
+
+function kinetic(N::Int, a::Array{Float64, 1}, σ::Float64)
+    d = N^2
+    s = 0.
+    for l = 1:d #<-- TODO: check this part
+        if (l>1) && (l < d+1-N)
+            s += abs2(a[l+N] - a[l]) + abs2(a[l-1] - a[l])
+        elseif l>1
+            s += abs2( - a[l]) + abs2(a[l-1] - a[l])
+        elseif l < d+1-N
+            s += abs2(a[l+N] - a[l]) + abs2( - a[l])
+        else 
+            s += abs2(- a[l]) + abs2(- a[l])
+        end
+    end ##
+    return σ*s/sum(abs2(a))
+end 
+
+# TODO: constant norm
+# TODO: plot energies vs time (kin, pot, tot)
+# TODO: try different lattice sizes
+
+
+function potential(N::Int, a::Array{Float64, 1}, σ::Float64, m₀::Int, n₀::Int)
+    s = 0.
+    d = N^2 
+    for l = 1:d
+        m = μ(l::Int,N::Int)
+        n = ν(l::Int,N::Int)
+
+        s += abs2(a[l])*cl(m,n,m₀,n₀)
+    end
+    return 1/σ*s/sum(abs2(a))
+end 
+
+kinetic(N, aout[end], 25.)
+
+potential(N, aout[end], 25., 0, 0)
+
 
 
 ### --->
@@ -77,5 +120,5 @@ plt.close(fig)
 
 ### <--- profiling
 ## Profile.clear()
-## @profile (for i=1:100;  F(1.,ones(Complex{Float64}, 512^2), 512,0,0,1.) ; end)
+## @profile (for i=1:100;  F(1.,ones(Float64, 512^2), 512,0,0,1.) ; end)
 ## Profile.print()
